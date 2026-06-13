163ff14d6c61973cfdb47c9386b5a9354fbca114
Joshiii03 <67600895+Joshiii03@users.noreply.github.com>
2026-06-13 23:03:07 +0200

    portals are travable

--- stencil-portals/portal/travable_portal.gd
+++ stencil-portals/portal/travable_portal.gd
@@ -1,15 +1,22 @@
 class_name TravablePortal
 extends Portal
 
-var portal_layer_1 : int = 1
-var portal_layer_2 : int = 2
+@export var portal_layer_2 : int = 2 :
+	set(value):
+			portal_layer_2 = value
+			update_portal_layer() 
 
 enum PortalLayers {portal_layer_1, portal_layer_2}
-var active_portal_layer : PortalLayers = PortalLayers.portal_layer_1
+# The active PortalLAyer is the on being displayed the other is th one the player is inside
+var active_portal_layer : PortalLayers = PortalLayers.portal_layer_1 :
+	set(value):
+		active_portal_layer = value
+		update_portal_layer() 
 
 @onready var portal_1a : PortalPlane = $Portal1A
 @onready var portal_1b : PortalPlane = $Portal1B
 
+# If the Player is insdie the Area3D
 var player_inside : bool = false :
 	set(new_value):
 		if new_value:
@@ -28,7 +35,33 @@ func _process(_delta: float) -> void:
 		last_player_side = player_side
 
 
+# Will be called when PortalLayer is changed
 func update_portal_layer() -> void:
+	if player_inside:
+		match active_portal_layer:
+			PortalLayers.portal_layer_1:
+				if player_side:
+					portal_1a.portal_layer = portal_layer_2
+					portal_1b.portal_layer = portal_layer
+				else:
+					portal_1a.portal_layer = portal_layer
+					portal_1b.portal_layer = portal_layer_2
+			PortalLayers.portal_layer_2:
+				if player_side:
+					portal_1a.portal_layer = portal_layer
+					portal_1b.portal_layer = portal_layer_2
+				else:
+					portal_1a.portal_layer = portal_layer_2
+					portal_1b.portal_layer = portal_layer
+	else:
+		match active_portal_layer:
+			PortalLayers.portal_layer_1:
+				portal_1a.portal_layer = portal_layer
+				portal_1b.portal_layer = portal_layer
+			PortalLayers.portal_layer_2:
+				portal_1a.portal_layer = portal_layer_2
+				portal_1b.portal_layer = portal_layer_2
+	
 	portal_1a.update_portal_layer()
 	portal_1b.update_portal_layer()
 
@@ -36,12 +69,26 @@ func update_portal_layer() -> void:
 func _on_area_3d_body_entered(body):
 	if body == player:
 		player_inside = true
+		print("player inside")
 
 
 func _on_area_3d_body_exited(body):
 	if body == player:
 		player_inside = false
+		update_portal_layer()
 
 
+# Is triggered when Player changes side of portal whil standing inside.
+# Swaps active PortalLayer
 func player_changed_side():
+	match active_portal_layer:
+		PortalLayers.portal_layer_1:
+			player.change_layer(portal_layer)
+			active_portal_layer = PortalLayers.portal_layer_2
+			
+		PortalLayers.portal_layer_2:
+			player.change_layer(portal_layer_2)
+			active_portal_layer = PortalLayers.portal_layer_1
+			
+	
 	print("Player changed side")
